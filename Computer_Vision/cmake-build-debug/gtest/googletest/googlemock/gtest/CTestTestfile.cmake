65af45c9152e836936bd133edb99aec7d2919ad9
Dextyh <tmark.boy001@gmail.com>
2019-01-30 12:40:30 +0100

    Computer Vision

--- Computer_Vision/cmake-build-debug/gtest/googletest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Dexty/Desktop/GreenFox/googletest/googletest
-# Build directory: /Users/Dexty/Desktop/Computer_Vision/cmake-build-debug/gtest/googletest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
